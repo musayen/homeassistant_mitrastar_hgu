e32a619469d8484588b97d69538caf484057c03d
rodriguezst <carlos@rodriguezst.es>
2019-11-25 22:56:59 +0100

    Code cleanup and new input parameter
    Added number_of_guest_ap as an input parameter for configuration.yaml to select the number of guest wireless networks enabled in the router.
    Default value is 0 (no guest networks)

--- MitraStar_GPT-2541GNAC/device_tracker.py
+++ MitraStar_GPT-2541GNAC/device_tracker.py
@@ -18,16 +18,20 @@ import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
+# Custom element for configuration.yaml
+# number_of_guest_ap: number of guest wireless networks enabled on the router
+CONF_NUMBER_GUEST_AP = 'number_of_guest_ap'
+DEFAULT_NUMBER_GUEST_AP = 0
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_HOST): cv.string,
     vol.Required(CONF_PASSWORD): cv.string,
-    vol.Required(CONF_USERNAME): cv.string
+    vol.Required(CONF_USERNAME): cv.string,
+    vol.Optional(CONF_NUMBER_GUEST_AP, default=DEFAULT_NUMBER_GUEST_AP): cv.positive_int
 })
 
-
 def get_scanner(hass, config):
-    # """Validate the configuration and return a TP-Link scanner."""
+    """Validate the configuration and return a Mitrastar scanner."""
 
     scanner = MitraStarDeviceScanner(config[DOMAIN])
     return scanner if scanner.success_init else None
@@ -41,6 +45,11 @@ class MitraStarDeviceScanner(DeviceScanner):
         host = config[CONF_HOST]
         username = config[CONF_USERNAME]
         password = config[CONF_PASSWORD]
+        n_guest_ap = config[CONF_NUMBER_GUEST_AP]
+        
+        # The maximum number of guest wireless APs for this router is 3
+        if (n_guest_ap > 3):
+            n_guest_ap = 3
 
         self.parse_macs = re.compile(r'([0-9a-fA-F]{2}:' + '[0-9a-fA-F]{2}:' + '[0-9a-fA-F]{2}:' + '[0-9a-fA-F]{2}:' + '[0-9a-fA-F]{2}:' + '[0-9a-fA-F]{2})')
         self.parse_dhcp = re.compile(r'<td>([0-9a-zA-Z\-._]+)<\/td><td>([0-9a-fA-F]{2}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2})<\/td><td>([0-9]+.[0-9]+.[0-9]+.[0-9]+.[0-9]+)')
@@ -48,12 +57,13 @@ class MitraStarDeviceScanner(DeviceScanner):
         self.host = host
         self.username = username
         self.password = password
+        self.n_guest_ap = n_guest_ap
 
         self.LOGIN_URL = 'http://{ip}/login-login.cgi'.format(**{'ip': self.host})
         self.headers1 = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'}
 
         self.last_results = {}
-        self.hostnames = {}
+        self.dhcp_data = {}
         self.success_init = self._update_info()
 
     def scan_devices(self):
@@ -63,8 +73,8 @@ class MitraStarDeviceScanner(DeviceScanner):
 
 
     def get_device_name(self, device):
-        """This router doesn't save the name of the wireless device."""
-        match = [element[0] for element in self.hostnames if element[1].lower()==device]
+        """Get the device name from the router DHCP table."""
+        match = [element[0] for element in self.dhcp_data if element[1].lower()==device]
         if len(match) > 0:
             device_name = match[0]
         else:
@@ -77,12 +87,12 @@ class MitraStarDeviceScanner(DeviceScanner):
         """
         _LOGGER.info('Checking MitraStar GPT-2541GNAC Router')
 
-        data, hostnames = self.get_MitraStar_info()
+        data, dhcp_data = self.get_MitraStar_info()
         if not data:
             return False
 
         self.last_results = data
-        self.hostnames = hostnames
+        self.dhcp_data = dhcp_data
         return True
 
     def _read_table(self, session, url):
@@ -91,16 +101,11 @@ class MitraStarDeviceScanner(DeviceScanner):
             response_string = str(response.content, "utf-8")
             return response_string
         else:
-            este_error = 'Error al conectar al Router desde la url: {}'.format(url)
-            _LOGGER.error(este_error)
-
-
+            _LOGGER.error('Unable to contact router at: {}'.format(url))
 
     def get_MitraStar_info(self):
         """Retrieve data from MitraStar GPT-2541GNAC Router."""
-
-        # headers2 = dict(referer=self.LOGIN_URL)
-
+        
         username1 = str(self.username)
         password1 = str(self.password)
 
@@ -115,46 +120,39 @@ class MitraStarDeviceScanner(DeviceScanner):
             'pass': ''
         }
 
-        # Creo la sesion y hago login
+        # Session creation and login
         session1 = requests.Session()
         login_response = session1.post(self.LOGIN_URL, data=data1, headers=self.headers1)
        
-        # Si conecta bien con el Router
-        #if str(login_response) == '<Response [200]>':
+        # Check HTTP result code after login (200 = login successful)
         if login_response.status_code == 200:
 
-            # # Pagina1
-            url1 = 'http://{}/wlextstationlist.cmd?action=view&wlSsidIdx=2'.format(self.host)
-            url2 = 'http://{}/wlextstationlist.cmd?action=view&wlSsidIdx=1'.format(self.host)
-            url3 = 'http://{}/arpview.cmd'.format(self.host)
-            url4 = 'http://{}/dhcpinfo.html'.format(self.host)
+            # Router tables URLs
+            urls = ['http://{}/arpview.cmd'.format(self.host), \
+            'http://{}/wlstationlist.cmd'.format(self.host), \
+            'http://{}/wlextstationlist.cmd?action=view&wlSsidIdx=1'.format(self.host) ]
+            
+            # Add URLs for guest APs depending on the number_of_guest_ap defined at configuration.yaml
+            for guest_ap_id in range (2, 2+self.n_guest_ap):
+                urls.append('http://{}/wlextstationlist.cmd?action=view&wlSsidIdx={}'.format(self.host, guest_ap_id))
+            
+            mac_array = []
+            
+            # Read urls one by one and append unique elements to mac_array
+            for url_address in urls:
+                url_result = self._read_table(session1, url_address).lower()
+                mac_array_new = self.parse_macs.findall(url_result)
+                mac_array.extend([element for element in mac_array_new if element not in mac_array])
 
-            result1 = self._read_table(session1, url1).lower()
-            MAC_Address1 = self.parse_macs.findall(result1)
+            # Read DHCP table to store hostnames and IP addresses of hosts
+            dhcp_url = 'http://{}/dhcpinfo.html'.format(self.host)
+            url_result = self._read_table(session1, dhcp_url).replace('\n ','').replace(' ','')
+            dhcp_data = self.parse_dhcp.findall(url_result)
 
-            result2 = self._read_table(session1, url2).lower()
-            MAC_Address2 = self.parse_macs.findall(result2)
-
-            result3 = self._read_table(session1, url3).lower()
-            MAC_Address3 = self.parse_macs.findall(result3)
-
-            result4 = self._read_table(session1, url4)
-            result4 = result4.replace('\n ','').replace(' ','')
-            hostnames = self.parse_dhcp.findall(result4)
-
-
-            # Lo Uno Todo y Borro Duplicados. Así medio raro....
-            MAC_Address1.extend([element for element in MAC_Address2 if element not in MAC_Address1])
-            MAC_Address1.extend([element for element in MAC_Address3 if element not in MAC_Address1])
-            _LOGGER.info('MitraStar GPT-2541GNAC Router: Found %d devices' %len(MAC_Address1))
-
+            _LOGGER.info('MitraStar GPT-2541GNAC found %d devices' %len(mac_array))
 
         else:
-            MAC_Address1 = None
+            mac_array = None
             _LOGGER.error('Error connecting to the router...')
 
-        # Cierro la sesion
-        #session1.close()
-
-        return MAC_Address1, hostnames
-
+        return mac_array, dhcp_data
